9b5dc2cb6e5284a87978fb6f8a94b1b92f578bc7
edik keshish <ekeshish@yahoo.com>
2020-03-29 10:14:27 -0700

    Test for exclusivity of connected components

--- src/UnitTest/Geometry/ColoredTriangleMesh.cpp
+++ src/UnitTest/Geometry/ColoredTriangleMesh.cpp
@@ -25,7 +25,6 @@
 // ----------------------------------------------------------------------------
 
 #include "Open3D/Open3D.h"
-#include "Open3D/Geometry/TriangleMesh.h"
 #include "TestUtility/UnitTest.h"
 
 using namespace open3d;
@@ -42,37 +41,13 @@ TEST(ColoredTriangleMesh, IdenticallyColoredConnectedComponents) {
         EXPECT_TRUE(false);
     }
 
-    // The model contains two distinct meshes resulting in
-    // two distinct connected components.
-    // We clone the model and paint it with a uniform color so that
-    // we can verify that the number of distinct components is 2.  
-    geometry::TriangleMesh uniformMesh(mesh.vertices_, mesh.triangles_);
-    uniformMesh.PaintUniformColor(Eigen::Vector3d(0.5, 0.5, 0.5));
-
-    // Get the connected components of the uniform model
-    uniformMesh.IdenticallyColoredConnectedComponents();
-    auto connected_components = uniformMesh.identically_colored_connected_components_list_;
-
-    EXPECT_TRUE(2 == connected_components.size());
-
-    std::vector<std::set<int>>::const_iterator uiter =
-            connected_components.begin();
-    EXPECT_TRUE(*uiter ==
-            std::set<int>({0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-                30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-                40, 41}));
-    std::advance(uiter, 1);
-    EXPECT_TRUE(*uiter ==
-                std::set<int>({42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
-                               53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-                               64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
-                               75, 76, 77, 78, 79, 80, 81, 82, 83}));
-
+    //
+    // Verify the accuracy of total counts
+    //
     // Get the connected components of the original model
     mesh.IdenticallyColoredConnectedComponents();
-    connected_components = mesh.identically_colored_connected_components_list_;
+    auto connected_components =
+            mesh.identically_colored_connected_components_list_;
 
     // Print connected_components in the specified format
     size_t coloredCount = 0;
@@ -87,4 +62,43 @@ TEST(ColoredTriangleMesh, IdenticallyColoredConnectedComponents) {
                      mesh.vertices_.size(), coloredCount);
 
     EXPECT_TRUE(coloredCount == mesh.vertices_.size());
+
+    //
+    // Verify that connected componenets are exclusive of each other
+    //
+    auto itr1 = connected_components.begin();
+    for (; itr1 != connected_components.end()-1; itr1++) {
+        auto itr2 = itr1 + 1;
+        for (auto num : *itr1) {
+            EXPECT_TRUE(itr2->find(num) == itr2->end());
+        }
+    }
+
+    // The model contains two distinct meshes resulting in
+    // two distinct connected components.
+    // We clone the model and paint it with a uniform color so that
+    // we can verify that the number of distinct components is 2.
+    geometry::TriangleMesh uniformMesh(mesh.vertices_, mesh.triangles_);
+    uniformMesh.PaintUniformColor(Eigen::Vector3d(0.5, 0.5, 0.5));
+
+    // Get the connected components of the uniform model
+    uniformMesh.IdenticallyColoredConnectedComponents();
+    connected_components =
+            uniformMesh.identically_colored_connected_components_list_;
+
+    EXPECT_TRUE(2 == connected_components.size());
+
+    std::vector<std::set<int>>::const_iterator uiter =
+            connected_components.begin();
+    EXPECT_TRUE(*uiter ==
+                std::set<int>({0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+                               11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                               22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+                               33, 34, 35, 36, 37, 38, 39, 40, 41}));
+    std::advance(uiter, 1);
+    EXPECT_TRUE(*uiter ==
+                std::set<int>({42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+                               53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+                               64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
+                               75, 76, 77, 78, 79, 80, 81, 82, 83}));
 }
